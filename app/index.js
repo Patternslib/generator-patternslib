49c442b7926741f4532d6c60cef928c3840c7c5a
Johannes Raggam <raggam-nl@adm.at>
2015-10-21 14:11:22 +0200

    tabstop = 4 for js files

--- app/index.js
+++ app/index.js
@@ -3,56 +3,69 @@ var yeoman = require('yeoman-generator');
 
 var PatternGenerator = yeoman.generators.Base.extend({
 
-  constructor: function () {
-    'use strict';
-    yeoman.generators.Base.apply(this, arguments);
+    constructor: function() {
+        'use strict';
+        yeoman.generators.Base.apply(this, arguments);
 
-    this.argument('app_name', { type: String, required: true });
-    this.appname = this.app_name || this.appname;
-    this.option('appPath', {
-      desc: 'Name of application directory',
-      type: 'String',
-      defaults: this.appname,
-      banner: 'some banner'
-    });
-    this.env.options.appPath = this.options.appPath;
-    this.config.set('appPath', this.env.options.appPath);
-  },
-
-  writing: {
-
-    setupBase: function () {
-      this.mkdir(this.env.options.appPath);
-      this.mkdir(this.env.options.appPath + '/src');
+        this.argument('app_name', {
+            type: String,
+            required: true
+        });
+        this.appname = this.app_name || this.appname;
+        this.option('appPath', {
+            desc: 'Name of application directory',
+            type: 'String',
+            defaults: this.appname,
+            banner: 'some banner'
+        });
+        this.env.options.appPath = this.options.appPath;
+        this.config.set('appPath', this.env.options.appPath);
     },
 
-    setupBower: function () {
-      this.template('bowerrc', this.env.options.appPath + '/.bowerrc');
-      this.copy('_bower.json', this.env.options.appPath + '/bower.json');
-    },
+    writing: {
 
-    setupNode: function () {
-      this.template('_package.json', this.env.options.appPath + '/package.json');
-    },
+        setupBase: function() {
+            this.mkdir(this.env.options.appPath);
+            this.mkdir(this.env.options.appPath + '/src');
+        },
 
-    setupDevEnv: function () {
-      this.copy('editorconfig', this.env.options.appPath + '/.editorconfig');
-      this.template('gitignore', this.env.options.appPath + '/.gitignore');
-      this.copy('jshintrc', this.env.options.appPath + '/.jshintrc');
-      this.copy('Makefile', this.env.options.appPath + '/Makefile');
-    },
+        setupBower: function() {
+            this.template('bowerrc', this.env.options.appPath +
+                '/.bowerrc');
+            this.copy('_bower.json', this.env.options.appPath +
+                '/bower.json');
+        },
 
-    setupIndex: function () {
-      this.indexFile = this.readFileAsString(path.join(this.sourceRoot(), '/index.html'));
-      this.indexFile = this.engine(this.indexFile, this);
-      this.write(this.env.options.appPath + '/index.html', this.indexFile);
-    },
+        setupNode: function() {
+            this.template('_package.json', this.env.options.appPath +
+                '/package.json');
+        },
 
-    setupApp: function () {
-      this.template("main.js",    this.env.options.appPath + '/main.js');
-      this.template("pattern.js", this.env.options.appPath + '/src/' + this.appname + '.js');
+        setupDevEnv: function() {
+            this.copy('editorconfig', this.env.options.appPath +
+                '/.editorconfig');
+            this.template('gitignore', this.env.options.appPath +
+                '/.gitignore');
+            this.copy('jshintrc', this.env.options.appPath +
+                '/.jshintrc');
+            this.copy('Makefile', this.env.options.appPath +
+                '/Makefile');
+        },
+
+        setupIndex: function() {
+            this.indexFile = this.readFileAsString(path.join(this.sourceRoot(),
+                '/index.html'));
+            this.indexFile = this.engine(this.indexFile, this);
+            this.write(this.env.options.appPath + '/index.html', this.indexFile);
+        },
+
+        setupApp: function() {
+            this.template("main.js", this.env.options.appPath +
+                '/main.js');
+            this.template("pattern.js", this.env.options.appPath +
+                '/src/' + this.appname + '.js');
+        }
     }
-  }
 });
 
 module.exports = PatternGenerator;
